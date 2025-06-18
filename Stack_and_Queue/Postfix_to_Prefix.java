dd3d2e4da2dbcb0aee45f16d2b64b6c7a762e334
Anushka <anushkasjadhav1@gmail.com>
2025-06-18 08:51:32 +0530

    stack questions

---
+++ Stack_and_Queue/Postfix_to_Prefix.java
@@ -0,0 +1,37 @@
+package Stack_and_Queue;
+import java.util.*;
+
+
+public class Postfix_to_Prefix {
+    static boolean isOperand(char ch){
+        if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z') || (ch>='0' && ch<='9')){
+            return true;
+        }
+        return false;
+    }
+
+    static String postfixtoprefix(String postfix){
+        int n  = postfix.length();
+        int i=0;
+        Stack<String> st = new Stack<>();
+        while(i<n){
+            char ch = postfix.charAt(i);
+            if(isOperand(ch)){
+                st.push(String.valueOf(ch));
+            }
+            else{
+                String op2 = st.pop();
+                String op1 = st.pop();
+                String temp = ch + op1 + op2;
+                st.push(temp);
+            }
+            i++;
+        }
+        return st.pop();
+    }
+
+    public static void main(String[] args) {
+        String postfix = "ab*c+";
+        System.out.println(postfixtoprefix(postfix));
+    }
+}
---
+++ Stack_and_Queue/PrefixtoPostfix.java
@@ -0,0 +1,31 @@
+package Stack_and_Queue;
+import java.util.*;
+
+public class PrefixtoPostfix {
+    public static void main(String[] args) {
+        String prefix = "*-A/BC-/AKL";
+        System.out.println(prefixtopostfix(prefix));
+    }
+    
+
+    static String prefixtopostfix(String prefix){
+        Stack<String> stack = new Stack<>();
+        int n = prefix.length();
+        int i = n-1;
+        while (i>=0) {
+            String c = String.valueOf(prefix.charAt(i));
+            if(Character.isLetterOrDigit(c.charAt(0))){
+                stack.push(c);
+            }
+            else{
+                String op1 = String.valueOf(stack.pop());
+                String op2 = String.valueOf(stack.pop());
+                String temp = op1+op2+c;
+                stack.push(temp);
+            }
+            i--;
+        }
+
+        return stack.pop();
+    }
+}
