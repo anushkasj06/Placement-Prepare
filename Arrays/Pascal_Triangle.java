c69f2d34ae7aa2c7356805110ef71e41ed797540
Anushka <anushkasjadhav1@gmail.com>
2025-09-03 23:14:19 +0530

    dp

--- Arrays/Pascal_Triangle.java
+++ Arrays/Pascal_Triangle.java
@@ -1,38 +1,39 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Pascal_Triangle {
-    public int rthcthelemenetinpascal(int r, int c){
+
+    public int rthcthelemenetinpascal(int r, int c) {
         int res = 1;
-        for(int i=0;i<r;i++){
+        for (int i = 0; i < c; i++) {
             res = res * (r - i);
             res = res / (i + 1);
         }
         return res;
     }
 
-    public ArrayList<Integer> rthrowinpascal(int r){
-        int ans =1;
+    public ArrayList<Integer> rthrowinpascal(int r) {
         ArrayList<Integer> res = new ArrayList<>();
-        for(int i=1;i<r + 1;i++){
-           ans *= (r-i);
-              ans /= i;
-              res.add(ans);
+        int ans = 1;
+        res.add(ans); 
+        for (int col = 1; col <= r; col++) {
+            ans = ans * (r - col + 1) / col;  
+            res.add(ans);
         }
         return res;
     }
 
     public ArrayList<ArrayList<Integer>> generate(int numRows) {
         ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
-        for(int i=0;i<numRows;i++){
-            ArrayList<Integer> temp = rthrowinpascal(i);
-            ans.add(temp);
+        for (int i = 0; i < numRows; i++) {
+            ans.add(rthrowinpascal(i));
         }
         return ans;
     }
+
     public static void main(String[] args) {
         Pascal_Triangle pt = new Pascal_Triangle();
-        int r = 4, c = 2; // Example row and column
+        int r = 4, c = 2; 
+
         System.out.println("Element at row " + r + " and column " + c + ": " + pt.rthcthelemenetinpascal(r, c));
 
         ArrayList<Integer> row = pt.rthrowinpascal(r);
@@ -42,4 +43,3 @@ public class Pascal_Triangle {
         System.out.println("Pascal's Triangle with 5 rows: " + triangle);
     }
 }
-
